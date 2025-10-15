a8835bcaed78ccd6d11598b83d31740fa5739809
Prebor97 <prebstamar@gmail.com>
2025-10-15 16:02:31 -0700

    added logs

--- src/main/java/com/prebor/stage1/ServiceClass.java
+++ src/main/java/com/prebor/stage1/ServiceClass.java
@@ -1,5 +1,7 @@
 package com.prebor.stage1;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -9,7 +11,7 @@ import java.time.format.DateTimeFormatter;
 
 @Service
 public class ServiceClass {
-
+    private static final Logger log = LoggerFactory.getLogger(ServiceClass.class);
     private static final String CAT_FACT_URL = "https://catfact.ninja/fact";
     private final RestTemplate restTemplate;
 
@@ -17,9 +19,11 @@ public class ServiceClass {
         this.restTemplate = restTemplate;
     }
     public ResponseDto getPersonalInfo() {
+        log.info("Getting Json for cat api..................................................................");
         CatFactResponse response = restTemplate.getForObject(CAT_FACT_URL, CatFactResponse.class);
-
+        log.info("Processing user info...............................................................");
         User user = new User("prebstamar@gmail.com", "Prebor Tamarakro", "Java, Springboot");
+        log.info("Returning response...........................................................");
         return new ResponseDto("success", user, OffsetDateTime.now(ZoneOffset.UTC)
                 .format(DateTimeFormatter.ISO_OFFSET_DATE_TIME), response.getFact());
     }
